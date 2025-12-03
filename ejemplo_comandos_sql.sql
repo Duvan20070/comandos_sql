16e622fb9b2cce8022dc4fc0ef4213b0c00f7e7e
Duvan Saavedra <duvanalejandrosaaaved@gmail.com>
2025-12-03 17:12:10 -0500

    trabajo finalizado SQL con comandos DDL

--- ejemplo_comandos_sql.sql
+++ ejemplo_comandos_sql.sql
@@ -7,3 +7,43 @@ CREATE DATABASE bdBorrar;
 
 -- DROP DATABASE elimina una base de datos
 DROP DATABASE bdBorrar;
+
+USE testDB;
+
+CREATE TABLE Persons(
+	per_id int,
+	per_last_name varchar(255),
+	per_first_name varchar(255),
+	per_address varchar(255),
+	per_city varchar(255)
+);
+
+-- DROP TABLE eliminar tabla persons
+DROP TABLE Persons;
+
+-- ALERT TABLE agregar una columna
+
+ALTER TABLE Persons
+ADD per_email varchar(255);
+
+
+
+-- DROP COLUMN eliminar columnas de una tabla
+ALTER TABLE Persons
+DROP COLUMN per_email;
+
+-- RENAME COLUMN renombrar una columna en una tabla
+ALTER TABLE Persons
+RENAME COLUMN per_address to per_phone;
+
+-- MODIFY COLUMN modificar el tipo de dato de una columna
+ALTER TABLE Persons
+MODIFY COLUMN per_phone varchar(15);
+
+
+
+
+
+
+
+    
